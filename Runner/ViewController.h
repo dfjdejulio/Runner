caf7519b37b331d1b385cf7f494b92df1540781a
Doug DeJulio <ddj@aisb.org>
2014-05-17 17:33:40 -0400

    Initial Commit

---
+++ Runner/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Runner
+//
+//  Created by Doug DeJulio on 5/17/14.
+//  Copyright (c) 2014 AISB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Runner/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Runner
+//
+//  Created by Doug DeJulio on 5/17/14.
+//  Copyright (c) 2014 AISB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

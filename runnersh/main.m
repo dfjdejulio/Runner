e139f61c7a218f2142754c9b0e4aad2194d4eacd
Doug DeJulio <ddj@aisb.org>
2014-05-22 22:40:50 -0400

    Now the command-line tool builds up a dictionary of commands, and uses the first argument to select one.

--- runnersh/main.m
+++ runnersh/main.m
@@ -8,16 +8,20 @@
 
 #import <Foundation/Foundation.h>
 #import "JSEvalCommand.h"
+#import "HelloSailorCommand.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        Command *cmd = [JSEvalCommand new];
-        for (int i = 1; i < argc; i++) {
-            cmd.input = @(argv[i]);
-            [cmd execute];
-            printf("%d: %s → %s\n", i, argv[i], cmd.output.UTF8String);
+        NSDictionary *cmds = @{
+                                   @"eval": [JSEvalCommand new],
+                                   @"hello": [HelloSailorCommand new],
+                                   };
+        for (int i = 2; i < argc; i++) {
+            [cmds[@(argv[1])] setInput:@(argv[i])];
+            [cmds[@(argv[1])] execute];
+            printf("%d: %s → %s\n", i, argv[i], [[cmds[@(argv[1])] output] UTF8String]);
         }
 
     }
